a4429865475361534b2fd2bec8adf09e30f34170
Tulio Henrique <tuliodvp@gmail.com>
2018-03-12 23:21:48 -0300

    starting with states

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
-	render() {
-		return <input />
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
 	}
 
+	render() {
+		return <input onChange={(event) => console.log(event.target.value)} />;
+	}
 }
 /** Alternative way to declare export **/
 // export default SearchBar;
